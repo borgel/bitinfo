10201c0d1cfd68e67e86bf723eb50c30c38db87a
Borgel <iborgel@gmail.com>
2020-05-06 22:21:01 -0700

    Take trailing args with clap

--- bitinfo/src/main.rs
+++ bitinfo/src/main.rs
@@ -1,3 +1,24 @@
+use clap::{Arg, App, AppSettings};
+
 fn main() {
-    println!("Hello, world!");
+   let options = App::new("Trailing args example")
+      .setting(AppSettings::TrailingVarArg)
+      // TODO more args
+      .arg(Arg::with_name("inputs")
+           .multiple(true)
+          )
+      .get_matches();
+
+   // FIXME rm
+   println!("Hello, world!");
+
+   println!("{:?}", options);
+   let to_decode: Vec<&str> = options.values_of("inputs").unwrap().collect();
+   println!("{:?}", to_decode);
+
+   for td in to_decode {
+      if let Ok(as_integer) = td.parse::<u32>() {
+         println!(" {:?} -> {}", td, as_integer);
+      }
+   }
 }
