add788f00dca417a430efe927178fe716c5f160a
Borgel <iborgel@gmail.com>
2020-05-08 23:52:22 -0700

    Recognise registers by name

--- bitinfo/src/main.rs
+++ bitinfo/src/main.rs
@@ -1,6 +1,10 @@
 use clap::{Arg, App, AppSettings};
 use parse_int::parse;
 use std::mem::size_of_val;
+use std::env;
+
+// TODO support more separators
+const SEPARATORS: &str = ":";
 
 fn main() {
    let app = App::new("Trailing args example")
@@ -15,22 +19,33 @@ fn main() {
    let to_decode: Vec<&str> = options.values_of("inputs").unwrap().collect();
    println!("{:?}", to_decode);
 
-   let mut decoded_one = false;
+   // TODO begin loading all .bitinfo files in PWD
+
    for td in to_decode {
+      if td.contains(SEPARATORS) {
+         let mut sp: Vec<&str> = td.split(SEPARATORS).collect();
+         let numeric_val = sp.pop().unwrap();
+         if let Ok(nv) = parse::<u32>(numeric_val) {
+            smart_decode(&sp.join(":"), nv);
+         }
+         continue;
+      }
       if let Ok(as_integer) = parse::<u32>(td) {
          print_bits(td, as_integer);
-         decoded_one = true;
       }
-   }
-   if !decoded_one {
-      println!("Sorry, that wasn't a number");
-      // TODO print this
-      // app.print_long_help();
    }
 }
 
+fn smart_decode(key: &str, number: u32) {
+   println!("\n{}: {:?}", key, number);
+
+   // TODO decode from .bitinfo
+}
+
 fn print_bits(raw_string: &str, number: u32) {
-   println!("\n\"{}\" -> {} {:#X} {:#b}", raw_string, number, number, number);
+   println!("\"{}\" -> {} {:#X} {:#b}", raw_string, number, number, number);
+
+   // see if we can find a config file for this bin
 
    let bits = 8 * size_of_val(&number);
    let mut number_to_eat = number;
@@ -44,5 +59,9 @@ fn print_bits(raw_string: &str, number: u32) {
          break;
       }
    }
+}
+
+fn find_config_for_name(bin_name: &str) {
+   // search up PWD looking for .bitinfo  files
 }
 
